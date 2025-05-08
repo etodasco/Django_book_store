00d8b1a2aa7e2213b5fa141c8345f3c13fbecd24
Eric Todasco <erictodasco@me.com>
2025-05-08 10:48:51 -0400

    added model.py classes and updated admin.py

--- book_outlet/models.py
+++ book_outlet/models.py
@@ -5,6 +5,16 @@ from django.utils.text import slugify
 
 # Create your models here.
 
+class Country(models.Model):
+    name = models.CharField(max_length=50)
+    code = models.CharField(max_length=2)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Countries"
+
 class Address(models.Model):
     street = models.CharField(max_length=50)
     postal_code = models.CharField(max_length=6)
@@ -34,6 +44,7 @@ class Book(models.Model):
     author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True, related_name="books")
     is_bestselling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True)
+    published_countries = models.ManyToManyField(Country)
 
     def get_absolute_url(self):
         return reverse("book-detail", args=[self.id])
